866ee9b28e03926ec86a281924e3db8d873f2aca
CodeRacon <michael.buschmann.86@gmail.com>
2024-04-23 01:05:32 +0200

    feat: added basic logic for gravity

---
+++ script.js
@@ -0,0 +1,55 @@
+const canvas = document.getElementById('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 512;
+
+const gravity = 0.25;
+
+class Player {
+	constructor(position) {
+		this.position = position;
+		this.velocity = {
+			x: 0,
+			y: 1,
+		};
+		this.height = 100;
+	}
+
+	draw() {
+		c.fillStyle = 'salmon';
+		c.fillRect(this.position.x, this.position.y, 100, this.height);
+	}
+
+	update() {
+		this.draw();
+		this.position.y += this.velocity.y;
+
+		if (this.position.y + this.height + this.velocity.y < canvas.height) {
+			this.velocity.y += gravity;
+		} else {
+			this.velocity.y = 0;
+		}
+	}
+}
+
+const player = new Player({
+	x: 0,
+	y: 0,
+});
+const player2 = new Player({
+	x: 200,
+	y: 50,
+});
+
+let y = 100;
+
+function animate() {
+	window.requestAnimationFrame(animate);
+	c.fillStyle = 'black';
+	c.fillRect(0, 0, canvas.width, canvas.height);
+	player.update();
+	player2.update();
+}
+
+animate();
