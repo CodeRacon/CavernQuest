cca5b2d5e158d5ca71b2884907c1cdc68d26ec87
CodeRacon <michael.buschmann.86@gmail.com>
2024-04-26 00:45:04 +0200

    feat: hitBox implementation to have collision-detection on the actual vissible charakter img, not the img itself with its wider boundries

--- js/classes/player.js
+++ js/classes/player.js
@@ -20,22 +20,52 @@ class Player extends Sprite {
 
 		this.collisionBlocks = collisionBlocks;
 		this.isGrounded = false;
+
+		this.hitbox = {
+			position: {
+				x: this.position.x,
+				y: this.position.y,
+			},
+			width: 64,
+			height: 64,
+		};
 	}
 
 	update() {
 		this.updateFrames();
+		this.updateHitbox();
 
-		c.fillStyle = '#00fffb6a';
-		c.fillRect(this.position.x, this.position.y, this.width, this.height);
+		// draws out image
+		// c.fillStyle = '#00fffb6a';
+		// c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+		// draws out image
+		// c.fillStyle = '#0004ff8c';
+		// c.fillRect(
+
+		// 	this.hitbox.position.x,
+		// 	this.hitbox.position.y,
+		// 	this.hitbox.width,
+		// 	this.hitbox.height
+		// );
 		this.draw();
 		this.position.x += this.velocity.x;
+		this.updateHitbox();
 		this.detectHorizontalCollision();
 		this.applyGravity();
+		this.updateHitbox();
 		this.detectVerticalCollision();
 	}
 
-	changeCollisionBlockColor(collisionBlock, color) {
-		// collisionBlock.color = color;
+	updateHitbox() {
+		this.hitbox = {
+			position: {
+				x: this.position.x + 372,
+				y: this.position.y + 248,
+			},
+			width: 280,
+			height: 548,
+		};
 	}
 
 	detectHorizontalCollision() {
@@ -44,26 +74,30 @@ class Player extends Sprite {
 
 			if (
 				collision({
-					object1: this,
+					object1: this.hitbox,
 					object2: collisionBlock,
 				})
 			) {
 				if (this.velocity.x > 0) {
 					this.velocity.x = 0;
-					this.position.x = collisionBlock.position.x - this.width - 0.01;
-					this.changeCollisionBlockColor(collisionBlock, 'blue');
+
+					const offset =
+						this.hitbox.position.x - this.position.x + this.hitbox.width;
+
+					this.position.x = collisionBlock.position.x - offset - 0.01;
 					break;
 				}
 
 				if (this.velocity.x < 0) {
 					this.velocity.x = 0;
+
+					const offset = this.hitbox.position.x - this.position.x;
+
 					this.position.x =
-						collisionBlock.position.x + collisionBlock.width + 0.01;
-					this.changeCollisionBlockColor(collisionBlock, 'blue');
+						collisionBlock.position.x + collisionBlock.width - offset + 0.01;
 					break;
 				}
 			} else {
-				this.changeCollisionBlockColor(collisionBlock, '#f5df6460');
 			}
 		}
 	}
@@ -79,14 +113,17 @@ class Player extends Sprite {
 
 			if (
 				collision({
-					object1: this,
+					object1: this.hitbox,
 					object2: collisionBlock,
 				})
 			) {
 				if (this.velocity.y > 0) {
 					this.velocity.y = 0;
-					this.position.y = collisionBlock.position.y - this.height - 0.01;
-					this.changeCollisionBlockColor(collisionBlock, 'red');
+
+					const offset =
+						this.hitbox.position.y - this.position.y + this.hitbox.height;
+
+					this.position.y = collisionBlock.position.y - offset - 0.01;
 					this.isGrounded = true;
 
 					break;
@@ -94,13 +131,14 @@ class Player extends Sprite {
 
 				if (this.velocity.y < 0) {
 					this.velocity.y = 0;
+
+					const offset = this.hitbox.position.y - this.position.y;
+
 					this.position.y =
-						collisionBlock.position.y + collisionBlock.height + 0.01;
-					this.changeCollisionBlockColor(collisionBlock, 'red');
+						collisionBlock.position.y + collisionBlock.height - offset + 0.01;
 					break;
 				}
 			} else {
-				this.changeCollisionBlockColor(collisionBlock, '#f5df6460');
 			}
 		}
 	}
