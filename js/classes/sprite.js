6397684b29411fda6e1d4370181e0d39959a49e0
CodeRacon <michael.buschmann.86@gmail.com>
2024-04-25 20:23:00 +0200

    feat: set up Sprite clas as parant of Player class; handing over parameters; added a cropBox for sprite-animation; added logic to update frames in a loop

--- js/classes/sprite.js
+++ js/classes/sprite.js
@@ -1,18 +1,62 @@
 class Sprite {
-	constructor({ position, imgSrc }) {
+	constructor({ position, imgSrc, frameRate = 1, frameBuffer = 6, scale = 1 }) {
 		this.position = position;
+		this.scale = scale;
+		this.loaded = false;
 		this.image = new Image();
+		this.image.onload = () => {
+			this.width = (this.image.width / this.frameRate) * this.scale;
+			this.height = this.image.height * this.scale;
+			this.loaded = true;
+		};
+
 		this.image.src = imgSrc;
+		this.frameRate = frameRate;
+		this.currentFrame = 0;
+		this.frameBuffer = frameBuffer;
+		this.elepsedFrames = 0;
 	}
 
 	draw() {
 		if (!this.image) {
 			return;
 		}
-		c.drawImage(this.image, this.position.x, this.position.y);
+
+		const cropBox = {
+			position: {
+				x: this.currentFrame * (this.image.width / this.frameRate),
+				y: 0,
+			},
+			width: this.image.width / this.frameRate,
+			height: this.image.height,
+		};
+
+		c.drawImage(
+			this.image,
+			cropBox.position.x,
+			cropBox.position.y,
+			cropBox.width,
+			cropBox.height,
+			this.position.x,
+			this.position.y,
+			this.width,
+			this.height
+		);
 	}
 
 	update() {
 		this.draw();
+		this.updateFrames;
+	}
+
+	updateFrames() {
+		this.elepsedFrames++;
+		if (this.elepsedFrames % this.frameBuffer === 0) {
+			if (this.currentFrame < this.frameRate - 1) {
+				this.currentFrame++;
+			} else {
+				this.currentFrame = 0;
+			}
+		}
 	}
 }
