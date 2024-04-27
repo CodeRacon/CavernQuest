fe111c51d96b4aa5079ace4651e871218c14d473
CodeRacon <michael.buschmann.86@gmail.com>
2024-04-27 22:30:47 +0200

    feat: #41 added hit-damage by HangingBlob

--- js/classes/hangingBlob.js
+++ js/classes/hangingBlob.js
@@ -12,16 +12,87 @@ class HangingBlob extends Sprite {
 			x: 0,
 			y: 1,
 		};
-		this.fencePoles = fencePoles;
-		this.direction = 1; // 1 für Bewegung nach rechts, -1 für Bewegung nach links
+
+		this.isAwake = false;
+		this.collisionCooldown = 0;
+
+		this.sensor = {
+			position: {
+				x: this.position.x,
+				y: this.position.y,
+			},
+			width: this.width * 1.25,
+			height: this.height * 5,
+		};
 	}
 
-	update() {
+	update(player) {
 		this.updateFrames();
 
-		// Überprüfe Kollision mit fencePoles
+		if (!this.isAwake) {
+			this.checkPlayerProximity(player);
+		} else {
+			this.position.y += this.velocity.y;
+			this.velocity.y += gravity;
+		}
 
-		// Rufe die update()-Methode der Elternklasse auf
+		this.sensor = {
+			position: {
+				x: this.position.x,
+				y: this.position.y,
+			},
+			width: this.width * 1.25,
+			height: this.height * 3.25,
+		};
+
+		c.fillStyle = '#00eeff8c';
+		c.fillRect(
+			this.sensor.position.x - 64,
+			this.sensor.position.y,
+			this.sensor.width,
+			this.sensor.height
+		);
+
+		if (this.isAwake) {
+			// Definiere den Offset für den sichtbaren Bereich des HangingBlobs
+			const offset = 110;
+
+			// Überprüfe die Kollision zwischen dem HangingBlob und dem Spieler
+			if (
+				player.hitbox.position.x + player.hitbox.width >=
+					this.position.x + offset &&
+				player.hitbox.position.x <= this.position.x + this.width - offset &&
+				player.hitbox.position.y + player.hitbox.height >= this.position.y &&
+				player.hitbox.position.y <= this.position.y + this.height
+			) {
+				// Überprüfe, ob der Kollisions-Cooldown abgelaufen ist
+				if (this.collisionCooldown <= 0) {
+					// Kollision erkannt, verringere die Lebenspunkte des Spielers
+					player.health -= 10;
+
+					// Setze den Kollisions-Cooldown auf einen bestimmten Wert, z.B. 60 Frames (ca. 1 Sekunde bei 60 FPS)
+					this.collisionCooldown = 60;
+				}
+			}
+		}
+
+		if (this.collisionCooldown > 0) {
+			this.collisionCooldown--;
+		}
+
 		super.update();
+	}
+
+	checkPlayerProximity(player) {
+		if (
+			player.hitbox.position.x + player.hitbox.width >=
+				this.sensor.position.x &&
+			player.hitbox.position.x <= this.sensor.position.x + this.sensor.width &&
+			player.hitbox.position.y + player.hitbox.height >=
+				this.sensor.position.y &&
+			player.hitbox.position.y <= this.sensor.position.y + this.sensor.height
+		) {
+			this.isAwake = true;
+		}
 	}
 }
