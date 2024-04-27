e565dfa474f534d7dd99fbaf7e3c3111e9d8552c
CodeRacon <michael.buschmann.86@gmail.com>
2024-04-27 22:21:54 +0200

    feat: #43 implemented logic to have BouncePlants act like a trampolin

---
+++ js/classes/bouncePlant.js
@@ -0,0 +1,22 @@
+class BouncePlant extends Sprite {
+	constructor({ position, imgSrc, frameRate, frameBuffer, scale = 1 }) {
+		super({
+			imgSrc,
+			frameRate,
+			frameBuffer,
+			scale,
+		});
+
+		this.position = position;
+		this.velocity = {
+			x: 0,
+			y: 1,
+		};
+	}
+
+	update() {
+		this.updateFrames();
+
+		super.update();
+	}
+}
