173fd05227326256af949d1b7d46861b793549fb
CodeRacon <michael.buschmann.86@gmail.com>
2024-04-29 23:43:14 +0200

    fix: moved collision-control over to Player class

--- js/classes/hazard.js
+++ js/classes/hazard.js
@@ -3,7 +3,6 @@ class Hazard {
 		this.position = position;
 		this.width = width;
 		this.height = height;
-		this.collisionCooldown = 0;
 	}
 
 	draw() {
@@ -12,30 +11,7 @@ class Hazard {
 		c.fillRect(this.position.x, this.position.y, this.width, this.height);
 	}
 
-	update(player) {
+	update() {
 		this.draw();
-
-		// check for collision of player and hazard-object
-		if (
-			player.hitbox.position.x + player.hitbox.width >= this.position.x &&
-			player.hitbox.position.x <= this.position.x + this.width &&
-			player.hitbox.position.y + player.hitbox.height >= this.position.y &&
-			player.hitbox.position.y <= this.position.y + this.height
-		) {
-			// chek for collision cooldown to be done
-			if (this.collisionCooldown <= 0) {
-				// detected collision, reduce player health by 2 points
-				player.health -= 2;
-
-				// set collision cooldown to 60 frames (= 1 Second)
-				this.collisionCooldown = 60;
-				player.takeDamage();
-			}
-		}
-
-		// reduce collision cooldown in each frame
-		if (this.collisionCooldown > 0) {
-			this.collisionCooldown--;
-		}
 	}
 }
