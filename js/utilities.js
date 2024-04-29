df727ca543f7999ceb32c9d18113f5bf5c7381b1
CodeRacon <michael.buschmann.86@gmail.com>
2024-04-29 20:42:09 +0200

    feat: #19 added preload function for all used sprites and images

--- js/utilities.js
+++ js/utilities.js
@@ -6,3 +6,26 @@ function collision({ object1, object2 }) {
 		object1.position.x + object1.width >= object2.position.x
 	);
 }
+
+function preloadImages(urls) {
+	urls.forEach(function (url) {
+		const img = new Image();
+		img.src = url;
+	});
+}
+
+const spriteImages = [
+	'img/wizard/magic-spell-left.png',
+	'img/wizard/magic-spell-right.png',
+	'img/wizard/Wizard-Fall-Left.png',
+	'img/wizard/Wizard-Fall-Right.png',
+	'img/wizard/wizard-hit-left.png',
+	'img/wizard/wizard-hit-right.png',
+	'img/wizard/Wizard-Idle-Left.png',
+	'img/wizard/Wizard-Idle-Right.png',
+	'img/wizard/Wizard-Jump-Left.png',
+	'img/wizard/Wizard-Jump-Right.png',
+	'img/wizard/Wizard-Walk-Left.png',
+	'img/wizard/Wizard-Walk-Right.png',
+	'img/background.jpg',
+];
