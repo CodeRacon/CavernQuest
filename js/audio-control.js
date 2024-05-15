80aa582536a01e2ba64205f6ecfed579858d627a
CodeRacon <michael.buschmann.86@gmail.com>
2024-05-15 22:24:30 +0200

    feat: #39 added background-sound; revised the play-audio functions to to better control over mute/unmute in certain modals

--- js/audio-control.js
+++ js/audio-control.js
@@ -1,12 +1,19 @@
 let isMuted = false;
 let mainStory;
 let lastPlayerPoisenedSound = 0;
+
 let isGameOverSoundPlayed = false;
+let isCarryOnPlayed = false;
+
+let isQuestCompletedPlayed = false;
+let isWellDonePlayed = false;
+
 let isHovering = false;
 let isDashing = false;
 let allTomesFound = false;
 let isAllTomesFoundPlayed = false;
-let isVictory = false;
+
+let isQuestCompleted = false;
 let isDead = false;
 
 function muteAllSounds() {
@@ -34,146 +41,205 @@ function playIntro() {
 	}
 }
 
+function playCavernBG() {
+	if (!isMuted) {
+		const cavernBGAudio = new Audio('./audio/cavern-bg-sound.mp3');
+		cavernBGAudio.volume = 0.075;
+		cavernBGAudio.play();
+		cavernBGAudio.loop = true;
+	}
+}
+
 function playPotionFound() {
-	const potionFoundAudio = new Audio('./audio/bottle.wav');
-	potionFoundAudio.volume = isMuted ? 0 : 0.2;
-	potionFoundAudio.play();
+	if (!isMuted) {
+		const potionFoundAudio = new Audio('./audio/bottle.wav');
+		potionFoundAudio.volume = 0.2;
+		potionFoundAudio.play();
+	}
 }
 
 function playPotionUse() {
-	const potionUseAudio = new Audio('./audio/use-potion.mp3');
-	potionUseAudio.volume = isMuted ? 0 : 0.45;
-	potionUseAudio.play();
+	if (!isMuted) {
+		const potionUseAudio = new Audio('./audio/use-potion.mp3');
+		potionUseAudio.volume = 0.35;
+		potionUseAudio.play();
+	}
 }
 
 function playMagicTomeFound() {
-	const tomeFoundAudio = new Audio('./audio/quest-item.mp3');
-	tomeFoundAudio.volume = isMuted ? 0 : 0.45;
-	tomeFoundAudio.play();
+	if (!isMuted) {
+		const tomeFoundAudio = new Audio('./audio/quest-item.mp3');
+		tomeFoundAudio.volume = 0.45;
+		tomeFoundAudio.play();
+	}
 }
 
 function playGemCollected() {
-	const gemCollectedAudio = new Audio('./audio/gem-collected.mp3');
-	gemCollectedAudio.volume = isMuted ? 0 : 0.25;
-	gemCollectedAudio.play();
+	if (!isMuted) {
+		const gemCollectedAudio = new Audio('./audio/gem-collected.mp3');
+		gemCollectedAudio.volume = 0.25;
+		gemCollectedAudio.play();
+	}
 }
 
 function playBounceSound() {
-	const bouncePlantAudio = new Audio('./audio/bounce.mp3');
-	bouncePlantAudio.volume = isMuted ? 0 : 0.175;
-	bouncePlantAudio.play();
+	if (!isMuted) {
+		const bouncePlantAudio = new Audio('./audio/bounce.mp3');
+		bouncePlantAudio.volume = 0.175;
+		bouncePlantAudio.play();
+	}
 }
 
 function playPlayerHurt() {
-	const playerHurtAudio = new Audio('./audio/player-hurt01.mp3');
-	playerHurtAudio.volume = isMuted ? 0 : 0.175;
-	playerHurtAudio.play();
+	if (!isMuted) {
+		const playerHurtAudio = new Audio('./audio/player-hurt01.mp3');
+		playerHurtAudio.volume = 0.175;
+		playerHurtAudio.play();
+	}
 }
 
 function playPlayerHurtAlt() {
-	const playerHurtAltAudio = new Audio('./audio/player-hurt02.mp3');
-	playerHurtAltAudio.volume = isMuted ? 0 : 0.175;
-	playerHurtAltAudio.play();
+	if (!isMuted) {
+		const playerHurtAltAudio = new Audio('./audio/player-hurt02.mp3');
+		playerHurtAltAudio.volume = 0.175;
+		playerHurtAltAudio.play();
+	}
 }
 
 function playPlayerPoisened() {
-	const timeStamp = Date.now();
-	const playerPoisonedAudio = new Audio('./audio/got-poisoned.mp3');
+	if (!isMuted) {
+		const timeStamp = Date.now();
+		const playerPoisonedAudio = new Audio('./audio/got-poisoned.mp3');
 
-	if (timeStamp - lastPlayerPoisenedSound > 4000) {
-		playerPoisonedAudio.volume = isMuted ? 0 : 0.2;
-		playerPoisonedAudio.play();
-		lastPlayerPoisenedSound = timeStamp;
+		if (timeStamp - lastPlayerPoisenedSound > 4000) {
+			playerPoisonedAudio.volume = 0.2;
+			playerPoisonedAudio.play();
+			lastPlayerPoisenedSound = timeStamp;
+		}
 	}
 }
 
 function playSpellcast() {
-	const playerSpellcastAudio = new Audio('./audio/spell-cast.mp3');
-	playerSpellcastAudio.volume = isMuted ? 0 : 0.25;
-	playerSpellcastAudio.play();
+	if (!isMuted) {
+		const playerSpellcastAudio = new Audio('./audio/spell-cast.mp3');
+		playerSpellcastAudio.volume = 0.25;
+		playerSpellcastAudio.play();
+	}
 }
 
 function playBlobFall() {
-	const blobFallAudio = new Audio('./audio/sleepy-blob.wav');
-	blobFallAudio.volume = isMuted ? 0 : 0.45;
-	blobFallAudio.play();
+	if (!isMuted) {
+		const blobFallAudio = new Audio('./audio/sleepy-blob.wav');
+		blobFallAudio.volume = 0.25;
+		blobFallAudio.play();
+	}
 }
 
 function playMovingBlob01() {
-	const movingBlob01Audio = new Audio('./audio/moving-blob01.mp3');
-	movingBlob01Audio.volume = isMuted ? 0 : 0.35;
-	setTimeout(() => {
-		movingBlob01Audio.play();
-	}, 750);
+	if (!isMuted) {
+		const movingBlob01Audio = new Audio('./audio/moving-blob01.mp3');
+		movingBlob01Audio.volume = 0.35;
+		setTimeout(() => {
+			movingBlob01Audio.play();
+		}, 750);
+	}
 }
 
 function playMovingBlob02() {
-	const movingBlob02Audio = new Audio('./audio/moving-blob02.mp3');
-	movingBlob02Audio.volume = isMuted ? 0 : 0.35;
-	setTimeout(() => {
-		movingBlob02Audio.play();
-	}, 750);
+	if (!isMuted) {
+		const movingBlob02Audio = new Audio('./audio/moving-blob02.mp3');
+		movingBlob02Audio.volume = 0.35;
+		setTimeout(() => {
+			movingBlob02Audio.play();
+		}, 750);
+	}
 }
 
 function playMovingBlob03() {
-	const movingBlob03Audio = new Audio('./audio/moving-blob03.mp3');
-	movingBlob03Audio.volume = isMuted ? 0 : 0.35;
-	setTimeout(() => {
-		movingBlob03Audio.play();
-	}, 750);
+	if (!isMuted) {
+		const movingBlob03Audio = new Audio('./audio/moving-blob03.mp3');
+		movingBlob03Audio.volume = 0.35;
+		setTimeout(() => {
+			movingBlob03Audio.play();
+		}, 750);
+	}
 }
 
 function playJump() {
-	const jumpAudio = new Audio('./audio/jump.mp3');
-	jumpAudio.volume = isMuted ? 0 : 0.35;
-	jumpAudio.play();
+	if (!isMuted) {
+		const jumpAudio = new Audio('./audio/jump.mp3');
+		jumpAudio.volume = 0.35;
+		jumpAudio.play();
+	}
 }
 
 function playHover() {
-	if (!isHovering) {
-		const hoverAudio = new Audio('./audio/hover.mp3');
-		hoverAudio.volume = isMuted ? 0 : 0.15;
-		hoverAudio.play();
-		isHovering = true;
+	if (!isMuted) {
+		if (!isHovering) {
+			const hoverAudio = new Audio('./audio/hover.mp3');
+			hoverAudio.volume = 0.15;
+			hoverAudio.play();
+			isHovering = true;
+		}
 	}
 }
 
 function playDash() {
-	if (!isDashing) {
-		const dashAudio = new Audio('./audio/dash.mp3');
-		dashAudio.volume = isMuted ? 0 : 0.15;
-		dashAudio.play();
-		isDashing = true;
+	if (!isMuted) {
+		if (!isDashing) {
+			const dashAudio = new Audio('./audio/dash.mp3');
+			dashAudio.volume = 0.15;
+			dashAudio.play();
+			isDashing = true;
+		}
 	}
 }
 
 function playScrollFound() {
-	const scrollFoundAudio = new Audio('./audio/scroll-found.mp3');
-	scrollFoundAudio.volume = isMuted ? 0 : 0.35;
-	scrollFoundAudio.play();
+	if (!isMuted) {
+		const scrollFoundAudio = new Audio('./audio/scroll-found.mp3');
+		scrollFoundAudio.volume = 0.35;
+		scrollFoundAudio.play();
+	}
 }
 
 function playAllTomesFound() {
-	if (!isAllTomesFoundPlayed) {
-		const allTomesAudio = new Audio('./audio/all-tomes.mp3');
-		allTomesAudio.volume = isMuted ? 0 : 0.35;
-		allTomesAudio.play();
-		isAllTomesFoundPlayed = true;
+	if (!isMuted) {
+		if (!isAllTomesFoundPlayed) {
+			const allTomesAudio = new Audio('./audio/all-tomes.mp3');
+			allTomesAudio.volume = 0.35;
+			allTomesAudio.play();
+			isAllTomesFoundPlayed = true;
+		}
 	}
 }
 
 function playQuestCompleted() {
-	const questCompletedAudio = new Audio('./audio/quest-completed.mp3');
-	questCompletedAudio.volume = isMuted ? 0 : 0.35;
-	questCompletedAudio.play();
+	const questCompletedAudio = new Audio('./audio/winning.mp3');
+	questCompletedAudio.volume = 0.35;
+	if (isQuestCompleted && !isQuestCompletedPlayed) {
+		questCompletedAudio.play();
+		isQuestCompletedPlayed = true;
+	}
 }
 
+let wellDoneAudio;
+
 function playWellDone() {
-	if (!isVictory) {
-		const wellDoneAudio = new Audio('./audio/well-done.mp3');
-		wellDoneAudio.volume = 0.65;
+	if (!wellDoneAudio) {
+		wellDoneAudio = new Audio('./audio/well-done.mp3');
+		wellDoneAudio.volume = 0.55;
+	}
+	if (!isWellDonePlayed && isQuestCompletedPlayed) {
 		wellDoneAudio.play();
-		isVictory = true;
+		isWellDonePlayed = true;
+	}
+}
+
+function stopWellDone() {
+	if (wellDoneAudio) {
+		wellDoneAudio.pause();
+		wellDoneAudio.currentTime = 0;
 	}
 }
 
@@ -186,27 +252,38 @@ function playGameOverSound() {
 	}
 }
 
-let isCarryOnPlayed = false;
+let carryOnAudio;
 
 function playCarryOn() {
-	if (!isCarryOnPlayed && isDead) {
-		const carryOnAudio = new Audio('./audio/carry-on.mp3');
-		isCarryOnPlayed = true;
+	if (!carryOnAudio) {
+		carryOnAudio = new Audio('./audio/carry-on.mp3');
 		carryOnAudio.volume = 0.65;
+	}
+	if (!isCarryOnPlayed && isGameOverSoundPlayed) {
 		carryOnAudio.play();
+		isCarryOnPlayed = true;
+	}
+}
+
+function stopCarryOn() {
+	if (carryOnAudio) {
+		carryOnAudio.pause();
+		carryOnAudio.currentTime = 0;
 	}
 }
 
 let heartbeatAudio;
 
 function playHeartBeat() {
-	if (!heartbeatAudio) {
-		heartbeatAudio = new Audio('./audio/heartbeat.mp3');
-		heartbeatAudio.volume = isMuted ? 0 : 0.75;
-		heartbeatAudio.loop = true;
-	}
-	if (heartbeatAudio.paused) {
-		heartbeatAudio.play();
+	if (!isMuted) {
+		if (!heartbeatAudio) {
+			heartbeatAudio = new Audio('./audio/heartbeat.mp3');
+			heartbeatAudio.volume = 0.75;
+			heartbeatAudio.loop = true;
+		}
+		if (heartbeatAudio.paused) {
+			heartbeatAudio.play();
+		}
 	}
 }
 
